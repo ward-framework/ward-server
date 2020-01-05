cec0bfc7522c23e82ede04dd521cd8a7925b2fff
ColinEspinas <colin.espinas@yahoo.fr>
2020-01-05 13:08:09 +0100

    Callback on serve and silent mode

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ function Server(options) {
 		path : __dirname, // Ward project path
 		port : 8000, // Server port
 		ip : '127.0.0.1', // Server ip
+		silent : false, // If false, log on success
 	}, options);
 
-	this.serve = function() {
+	this.serve = function(callback) {
 
 		// Serve up core folder
 		const serveCore = serveStatic(options.path + '/core', { 'index': false });
@@ -34,7 +35,9 @@ function Server(options) {
 		
 		// Listen to port and ip
 		app.listen(options.port, options.ip, function(){
-			console.log(chalk.blue.bold('Ward') + ' is running at ' + chalk.green.underline(`http://${options.ip}:${options.port}`));
+			if (!silent) 
+				console.log(chalk.blue.bold('Ward') + ' is running at ' + chalk.green.underline(`http://${options.ip}:${options.port}`));
+			callback();
 		});
 	}
 }
